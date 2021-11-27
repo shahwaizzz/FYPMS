7e6d99481616fd34ce28ceecce2699717ec882fb
AliAkbar001 <786aliakbar.aa@gmail.com>
2021-11-27 14:52:45 +0500

    Student-Commit

--- controllers/pmo-controller.js
+++ controllers/pmo-controller.js
@@ -2,29 +2,22 @@ const PMO = require("../models/pmo-model");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, UnauthenticatedError } = require("../errors");
 
-// const register = async (req, res) => {
-//   const pmo = await PMO.create({ ...req.body })
-//   // const token = pmo.createJWT()
-//   res.status(StatusCodes.CREATED).json({ pmo: { name: pmo.name } })
-// }
+const createStudent = async (req, res) => {
+  res.send(req.body);
+}
 
 const login = async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
     throw new BadRequestError("Please Provide Email and Password");
   }
-  console.log('log1')
+
   const pmo = await PMO.findOne({ email });
-  console.log('log2')
   if (!pmo) {
-    console.log(req.body);
     throw new UnauthenticatedError("Invalid Credentials");
   }
-  console.log('log3')
   const isPasswordCorrect = await pmo.comparePassword(password);
-  console.log('log4')
   if (!isPasswordCorrect) {
-    console.log('log5')
     throw new UnauthenticatedError("Your password does not match");
   }
   // const token = pmo.createJWT();
@@ -33,4 +26,5 @@ const login = async (req, res) => {
 
 module.exports = {
   login, 
+  createStudent,
 };
--- routes/pmo-routes.js
+++ routes/pmo-routes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { login } = require("../controllers/pmo-controller");
+const { login,createStudent } = require("../controllers/pmo-controller");
 
 // router.post('/register', register)
 router.post("/login", login);
-// router.post('/register', register)
+router.post('/student', createStudent)
 
 module.exports = router;
