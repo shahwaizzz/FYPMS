9fa29ce1929f4a3ddc7a03d0ab323f04d9576481
Shahwaiz Naveed <shahwaiznaveed46@gmail.com>
2021-11-27 03:20:57 -0800

    View Supervisors

--- controllers/pmo-controller.js
+++ controllers/pmo-controller.js
@@ -4,8 +4,6 @@ const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, UnauthenticatedError } = require("../errors");
 
 const Student = require("../models/student-model");
-const { StatusCodes } = require("http-status-codes");
-const { BadRequestError, UnauthenticatedError } = require("../errors");
 
 const createStudent = async (req, res) => {
   const std = await Student.create({ ...req.body });
@@ -38,8 +36,14 @@ const createSupervisors = async (req, res) => {
     .json({ supervisor: { name: supervisor.name } });
 };
 
+const viewSupervisors = async (req, res) => {
+  const supervisor = await Supervisor.find({});
+  res.status(StatusCodes.OK).json({ supervisor });
+};
+
 module.exports = {
   login,
   createSupervisors,
   createStudent,
+  viewSupervisors,
 };
--- routes/pmo-routes.js
+++ routes/pmo-routes.js
@@ -5,10 +5,12 @@ const {
   login,
   createSupervisors,
   createStudent,
+  viewSupervisors,
 } = require("../controllers/pmo-controller");
 
 router.post("/login", login);
 router.post("/create-supervisor", createSupervisors);
 router.post("/student", createStudent);
+router.get("/supervisors", viewSupervisors);
 
 module.exports = router;
