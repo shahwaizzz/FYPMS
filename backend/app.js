1d987b57f2ebe121edbd7fde4e1c5c1370238ce2
Safi <safiullahmaaz59@gmail.com>
2021-12-26 13:18:06 +0500

    some message

--- backend/app.js
+++ backend/app.js
@@ -44,7 +44,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URL);
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
