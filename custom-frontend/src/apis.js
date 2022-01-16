25975299cb849e0036fa757b578cade2a0618852
AliAkbar001 <786aliakbar.aa@gmail.com>
2022-01-17 02:21:24 +0500

    supervisor-start

--- custom-frontend/src/apis.js
+++ custom-frontend/src/apis.js
@@ -2,5 +2,5 @@ const port = 3000;
 const localhost = `localhost:${port}`;
 export const students = `http://${localhost}/api/v1/pmo/students`;
 export const supervisorsUrl = `http://${localhost}/api/v1/pmo/supervisors`;
-export const events = `http://${localhost}/api/v1/pmo/events`;
+export const eventUrl = `http://${localhost}/api/v1/pmo/events`;
 export const projects = `http://${localhost}/api/v1/pmo/projects`;
--- custom-frontend/src/components/main-components/events.js
+++ custom-frontend/src/components/main-components/events.js
@@ -1,40 +1,33 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { eventUrl } from "../../apis";
+import axios from 'axios';
+
 export default function Events() {
   const [getData, setGetData] = useState(false);
-  //   const [getEvents, setGetEvents] = useState(false);
-  //   const url = "http://localhost:3000/api/v1/pmo/events";
-  //   const api = axios.create({
-  //     baseURL: "http://localhost:3000/api/v1/pmo/events",
-  //   });
-  // useEffect(() => {
-  //   fetch("http://localhost:3000/api/v1/pmo/events")
-  //     .then((res) => res.json())
-  //     .then((resp) => {
-  //       setGetData(resp.events);
-  //       console.log(getData);
-  //     })
-  //     .catch((err) => console.log(err));
-  // });
-  //   useEffect(() => {
-  //     api
-  //       .get("/")
-  //       .then((res) => {
-  //         setGetEvents(res.events);
-  //       })
-  //       .catch((res) => {
-  //         alert(res);
-  //       });
-  //   }, []);
+// useEffect(() => {
+//   fetch(eventUrl)
+//   .then(res => res.json())
+//   .then((result) => {
+//     console.log(result);
+//     setGetData(result.events);
+//     console.log(getData);
+//   },(error) => {
+//       alert(error);
+// });
+// }, []);
+const api = axios.create({
+  baseURL : eventUrl
+});
 
+useEffect(() => {
+api.get("/")
+.then(res =>  {setGetData(res.events);console.log(getData)})
+.catch(res => {alert(res)})
+},[]);
   return (
     <div>
       <button className='btn'>Create Event</button>
-      <br />
-      {/* {getData.map((e) => {
-        <h4>{e.name}</h4>;
-      })}
-      al */}
+      {/* {getData&&getData.} */}
     </div>
   );
 }
