6252e85435eddc883fc078e83aef8ad56c5693ba
Shahwaiz Naveed <shahwaiznaveed46@gmail.com>
2022-01-18 06:30:11 -0800

    implemented react-router-dom

--- custom-frontend/src/App.js
+++ custom-frontend/src/App.js
@@ -1,28 +1,66 @@
-import React, { useState } from 'react'
-import Dashboard from './dashboard';
-import './index.css';
-import Login from "./login"
+import React, { useState } from "react";
+import Home from "./components/main-components/home";
+import Navbar from "./components/navbar";
+import Sidebar from "./components/sidebar";
+import Dashboard from "./dashboard";
+import "./index.css";
+import Login from "./login";
+import { Routes, Route } from "react-router-dom";
+import Projects from "./components/main-components/projects";
+import Events from "./components/main-components/events";
+import { students } from "./apis";
+import Student from "./components/main-components/student";
+import Supervisor from "./components/main-components/supervisor";
+import UserProfile from "./components/main-components/user-profile";
 
-function handleValidation(params) {
-  
-}
+function handleValidation(params) {}
 function App() {
   // Commit Test
   const [validation, setValidation] = useState(true);
-  if(validation){
+  if (validation) {
     return (
-      <div className="App">
-        <Dashboard/>
+      <div className='App'>
+        <Navbar />
+        <Routes>
+          <Route
+            path='/admin'
+            element={<Sidebar abc={<Home />} name='Home' />}
+          />
+          <Route
+            path='/admin/projects'
+            element={<Sidebar name='Projects' abc={<Projects />} />}
+          />
+          <Route
+            path='/admin/events'
+            element={<Sidebar name='Events' abc={<Events />} />}
+          />
+          <Route
+            path='/admin/students'
+            element={<Sidebar name='Students' abc={<Student />} />}
+          />
+          <Route
+            path='/admin/supervisors'
+            element={<Sidebar name='Supervisors' abc={<Supervisor />} />}
+          />
+          <Route
+            path='/admin/profile'
+            element={<Sidebar name='User Profile' abc={<UserProfile />} />}
+          />
+        </Routes>
+
+        {/* <Dashboard /> */}
+        {/* <Navbar />
+        <Sidebar abc={<Home />} /> */}
+        {/* <Home /> */}
       </div>
     );
-  }else{
+  } else {
     return (
-      <div className="App">
-        <Login/>
+      <div className='App'>
+        <Login />
       </div>
     );
   }
-  
 }
 
 export default App;
--- custom-frontend/src/components/sidebar.js
+++ custom-frontend/src/components/sidebar.js
@@ -1,115 +1,166 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { IconContext } from "react-icons";
-import { AiFillLeftCircle, AiOutlineHome, AiOutlineBook, AiOutlineProfile,AiOutlineProject} from "react-icons/ai";
-import { MdOutlineSupervisedUserCircle ,MdLogout,MdOutlineEmojiEvents} from "react-icons/md";
-import Home from './main-components/home';
-import Student from './main-components/student';
-import Supervisor from './main-components/supervisor';
-import UserProfile from './main-components/user-profile';
-import Logout from './main-components/logout';
-import Projects from './main-components/projects';
-import Events from './main-components/events';
+import {
+  AiFillLeftCircle,
+  AiOutlineHome,
+  AiOutlineBook,
+  AiOutlineProfile,
+  AiOutlineProject,
+} from "react-icons/ai";
+import {
+  MdOutlineSupervisedUserCircle,
+  MdLogout,
+  MdOutlineEmojiEvents,
+} from "react-icons/md";
+import Home from "./main-components/home";
+import Student from "./main-components/student";
+import Supervisor from "./main-components/supervisor";
+import UserProfile from "./main-components/user-profile";
+import Logout from "./main-components/logout";
+import Projects from "./main-components/projects";
+import Events from "./main-components/events";
+import { Link } from "react-router-dom";
 
-export default function Sidebar() {
-    const [changePages, setChangePages] = useState(<Home/>);
-    const [menuChange, setMenuChange] = useState("menu-hide");
-    const [dataWidth, setDataWidth] = useState("data-width");
-    const [menubarList, setMenubarList] = useState("menubar-list");
-    const [pageName, setPageName] = useState("Home");
-  
-    function changePage(e) {
-        if(e==="Home"){
-            setPageName("Home");
-            setChangePages(<Home/>);
-        }
-        if(e==="Projects"){
-            setPageName("Projects");
-            setChangePages(<Projects/>);
-        }
-        if(e==="Events"){
-            setPageName("Events");
-            setChangePages(<Events/>);
-        }
-        if(e==="Student"){
-            setPageName("Students");
-            setChangePages(<Student/>);
-        }
-        if(e==="Supervisor"){
-            setPageName("Supervisors");
-            setChangePages(<Supervisor/>);
-        }
-        if(e==="UserProfile"){
-            setPageName("User Profile");
-            setChangePages(<UserProfile/>);
-        }
-        if(e==="Logout"){
-            setPageName("Logout");
-            setChangePages(<Logout/>); 
-        }   
-        menuToggle("sidebar");  
-    }
+export default function Sidebar(props) {
+  const [changePages, setChangePages] = useState(props.abc);
+  const [menuChange, setMenuChange] = useState("menu-hide");
+  const [dataWidth, setDataWidth] = useState("data-width");
+  const [menubarList, setMenubarList] = useState("menubar-list");
+  const [pageName, setPageName] = useState(props.page);
 
-    function menuToggle(e){
-      if((menuChange==="menu-show")&&(dataWidth===null)){
-        setMenuChange("menu-hide");
-        setDataWidth("data-width");
-        setMenubarList("menubar-list");
-      }else if(e==="sidebar"){
-        setMenuChange("menu-hide");
-      }
-      else{
-          setMenuChange("menu-show");
-          setDataWidth(null);
-          setMenubarList(null);
-      }
+  //   function changePage(e) {
+  //     if (e === "Home") {
+  //       setPageName("Home");
+  //       setChangePages(<Home />);
+  //     }
+  //     if (e === "Projects") {
+  //       setPageName("Projects");
+  //       setChangePages(<Projects />);
+  //     }
+  //     if (e === "Events") {
+  //       setPageName("Events");
+  //       setChangePages(<Events />);
+  //     }
+  //     if (e === "Student") {
+  //       setPageName("Students");
+  //       setChangePages(<Student />);
+  //     }
+  //     if (e === "Supervisor") {
+  //       setPageName("Supervisors");
+  //       setChangePages(<Supervisor />);
+  //     }
+  //     if (e === "UserProfile") {
+  //       setPageName("User Profile");
+  //       setChangePages(<UserProfile />);
+  //     }
+  //     if (e === "Logout") {
+  //       setPageName("Logout");
+  //       setChangePages(<Logout />);
+  //     }
+  //     menuToggle("sidebar");
+  //   }
+
+  function menuToggle(e) {
+    if (menuChange === "menu-show" && dataWidth === null) {
+      setMenuChange("menu-hide");
+      setDataWidth("data-width");
+      setMenubarList("menubar-list");
+    } else if (e === "sidebar") {
+      setMenuChange("menu-hide");
+    } else {
+      setMenuChange("menu-show");
+      setDataWidth(null);
+      setMenubarList(null);
     }
-    return (
+  }
+  return (
     <div>
-        <div  className={"menubar " + menuChange}>
-        <div className={ menuChange }>
-            <IconContext.Provider value={{className:"icons menubar-icons", size:"1.5rem"}}>
-            <h2 className="menubar-title">
-                <span className="menubar-title-icon" onClick={()=>menuToggle()}>
-                    <AiFillLeftCircle size="2rem"/>
-                </span>
-                <span className="menubar-title-test">{pageName}</span>
+      <div className={"menubar " + menuChange}>
+        <div className={menuChange}>
+          <IconContext.Provider
+            value={{ className: "icons menubar-icons", size: "1.5rem" }}
+          >
+            <h2 className='menubar-title'>
+              <span className='menubar-title-icon' onClick={() => menuToggle()}>
+                <AiFillLeftCircle size='2rem' />
+              </span>
+              <span className='menubar-title-test'>{props.name}</span>
             </h2>
-            <ul className={ menubarList }>
-                <li onClick={()=>changePage("Home")}>
-                    <AiOutlineHome/>
-                    <span>Home</span>
+            <ul className={menubarList}>
+              {/* <li onClick={() => changePage("Home")}> */}
+              <Link to='/admin' className='links'>
+                <li>
+                  <AiOutlineHome />
+                  <span>Home</span>
                 </li>
-                <li onClick={()=>changePage("Projects")}>
-                    <AiOutlineProject/>
-                    <span>Manage Projects</span>
+              </Link>
+              <Link to='/admin/projects' className='links'>
+                <li>
+                  <AiOutlineProject />
+                  <span>Manage Projects</span>
                 </li>
-                <li onClick={()=>changePage("Events")}>
-                    <MdOutlineEmojiEvents/>
-                    <span>Mange Events</span>
+              </Link>
+              <Link to='/admin/events' className='links'>
+                <li>
+                  <MdOutlineEmojiEvents />
+                  <span>Events</span>
                 </li>
-                <li onClick={()=>changePage("Student")}>
-                    <AiOutlineBook/>
-                    <span>Students</span>
+              </Link>
+              <Link to='/admin/students' className='links'>
+                <li>
+                  <AiOutlineBook />
+                  <span>Students</span>
                 </li>
-                <li onClick={()=>changePage("Supervisor")}>
-                    <MdOutlineSupervisedUserCircle/>
-                    <span>Supervisors</span>
+              </Link>
+              <Link to='/admin/supervisors' className='links'>
+                <li>
+                  <MdOutlineSupervisedUserCircle />
+                  <span>Supervisors</span>
                 </li>
-                <li onClick={()=>changePage("UserProfile")}>
-                    <AiOutlineProfile/>
-                    <span>User Profile</span>
+              </Link>
+              <Link to='/admin/profile' className='links'>
+                <li>
+                  <AiOutlineProfile />
+                  <span>User Profile</span>
                 </li>
-                <li onClick={()=>changePage("Logout")}>
-                    <MdLogout/>
-                    <span> Logout</span>
+              </Link>
+              <Link to='/admin/logout' className='links'>
+                <li>
+                  <MdLogout />
+                  <span>Logout</span>
                 </li>
+              </Link>
+
+              {/* <li onClick={() => changePage("Projects")}>
+                <AiOutlineProject />
+                <span>Manage Projects</span>
+              </li>
+              <li onClick={() => changePage("Events")}>
+                <MdOutlineEmojiEvents />
+                <span>Mange Events</span>
+              </li>
+              <li onClick={() => changePage("Student")}>
+                <AiOutlineBook />
+                <span>Students</span>
+              </li>
+              <li onClick={() => changePage("Supervisor")}>
+                <MdOutlineSupervisedUserCircle />
+                <span>Supervisors</span>
+              </li>
+              <li onClick={() => changePage("UserProfile")}>
+                <AiOutlineProfile />
+                <span>User Profile</span>
+              </li>
+              <li onClick={() => changePage("Logout")}>
+                <MdLogout />
+                <span> Logout</span>
+              </li> */}
             </ul>
-            </IconContext.Provider>
+          </IconContext.Provider>
         </div>
-        </div>
-        <div className={"data " + dataWidth}>
-            {changePages}
-        </div>
+      </div>
+
+      <div className={"data " + dataWidth}>{props.abc}</div>
     </div>
-    )
+  );
 }
