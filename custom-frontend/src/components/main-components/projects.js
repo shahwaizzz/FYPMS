2f141e93f7efa9441bd1e5be7c299dd66539adf0
AliAkbar001 <786aliakbar.aa@gmail.com>
2022-01-19 19:30:04 +0500

    update-project-finish

--- custom-frontend/src/components/main-components/projects.js
+++ custom-frontend/src/components/main-components/projects.js
@@ -14,6 +14,7 @@ const [supervisorData, setSupervisorData] = useState(false);
 const [studentData, setStudentData] = useState(false);
 const [editForm, setEditForm] = useState(false);
 const [displayData, setDisplayData] = useState(false);
+const [refresh, setRefresh] = useState(false);
 
   const api = axios.create({
     baseURL: projectUrl,
@@ -47,15 +48,89 @@ const [displayData, setDisplayData] = useState(false);
       .catch((err) => {
         alert(err);
       });
-  }, []);
+  },[refresh]);
 
   function toggleModel(project) {
-    setDisplayData(project);
+    if(project !== null){
+      var groupMembers = [];
+
+      project.group.map(member =>
+        (studentData.map(std=> member === std._id && (
+          groupMembers = [...groupMembers,std.rollNumber]
+        )))
+        )
+  
+        var supervisorName = "";
+        supervisorData.map(e => project.supervisor === e._id && (supervisorName = e.name));
+  
+      setDisplayData({
+        _id:project._id,
+        title:project.title,
+        status:project.status,
+        description:project.description,
+        objectives:project.objectives,
+        supervisor:supervisorName,
+        member_1:groupMembers[0],
+        member_2:groupMembers[1],
+        member_3:groupMembers[2],
+      });
+    }
     editForm ? setEditForm(false) : setEditForm(true);
     }
   
   function handleSearch(){}
-  function handleChange(){}
+  function handleChange(e){
+    const name = e.target.name;
+    var value = e.target.value;
+    setDisplayData({ ...displayData, [name]: value });
+  }
+  function handleSubmit(e){
+    e.preventDefault();
+    var supervisorID;
+    supervisorData.map(e => e.name === displayData.supervisor && (supervisorID = e._id))
+    console.log(supervisorID)
+
+    var manageGroup = [displayData.member_1];
+    if(displayData.member_2 !== undefined ){
+      manageGroup = [...manageGroup,displayData.member_2]
+    }
+    if(displayData.member_3 !== undefined ){
+      manageGroup = [...manageGroup,displayData.member_3]
+    }
+    console.log(manageGroup);
+
+    var putMemberID = [];
+    manageGroup.map(rollNumber => (
+      studentData.map(std => rollNumber === std.rollNumber && ( putMemberID = [...putMemberID, std._id] ))
+    ))
+    console.log(putMemberID);
+    const putProject = {
+      _id:displayData._id,
+      title:displayData.title,
+      status:displayData.status,
+      description:displayData.description,
+      objectives:displayData.objectives,
+      supervisor:supervisorID,
+      group:putMemberID,
+    } 
+    console.log(putProject);
+    const options = {
+      method: "put",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(putProject),
+    };
+    fetch(`${projectUrl}/${putProject._id}`, options)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+          setRefresh(!refresh);
+        },
+        (error) => {
+          alert(error);
+        }
+      );
+  }
   return (
     <div className='data-container'>
       <div className='data-container-top'>
@@ -113,7 +188,7 @@ const [displayData, setDisplayData] = useState(false);
             <div className='form-modal'>
               <form
                 className='data-form'
-                // onSubmit={onSubmit}
+                onSubmit={handleSubmit}
                 autoComplete='off'
                 id='student-form'
               >
@@ -126,6 +201,16 @@ const [displayData, setDisplayData] = useState(false);
                     value={displayData.title}
                     onChange={handleChange}
                   />
+                </div>
+                <div>
+                  <label>Status</label>
+                  <select name="status" value={displayData.status} onChange={handleChange}>
+                    <option value="Pending">Pending</option>
+                    <option value="Rejected">Rejected</option>
+                    <option value="Approved">Approved</option>
+                    <option value="Working">Working</option>
+                    <option value="Complete">Complete</option>
+                  </select>
                 </div>
                  <div>
                   <label>Description</label>
@@ -140,50 +225,79 @@ const [displayData, setDisplayData] = useState(false);
                   <label>Objective</label>
                   <input
                     type='text'
-                    name='objective'
+                    name='objectives'
                     value={displayData.objectives}
                     onChange={handleChange}
                   />
                 </div>
                 <div>
                   <label>Supervisor</label>
-                  {supervisorData && supervisorData.map(e => displayData.supervisor === e._id &&
                     <input
                     type='text'
                     name='supervisor'
-                    value={e.name}
+                    value={displayData.supervisor}
+                    list = "supervisor-list"
                     onChange={handleChange}
                   />
-                  )}  
+                  <datalist id="supervisor-list">
+                    {supervisorData.map(e =>
+                      <option>{e.name}</option>
+                    )}
+                  </datalist>
                 </div>
-               
-                  {displayData.group.map(group => (
-                  studentData && studentData.map(e=> group === e._id && (
-                    <div>
-                    <label>Group Member</label>
-                    <input
-                    type='text'
-                    name=''
-                    value={e.rollNumber}
-                    onChange={handleChange}
-                  />
-                  </div>
-                  ))))}
-                  
-                 
+                  <div>
+                  <label>Group Leader</label>
+                  <input
+                  type='text'
+                  name='member_1'
+                  value={displayData.member_1}
+                  onChange={handleChange}
+                  list="student-list"
+                  placeholder="None"
+                  required
+                />
+                </div>
                 <div>
-                  <button type='submit'>Edit Supervisor</button>
+                  <label>Group Member</label>
+                  <input
+                  type='text'
+                  name='member_2'
+                  value={displayData.member_2}
+                  onChange={handleChange}
+                  list="student-list"
+                  placeholder="None"
+                />
+                </div>
+                <div>
+                  <label>Group Member</label>
+                  <input
+                  type='text'
+                  name='member_3'
+                  value={displayData.member_3}
+                  onChange={handleChange}
+                  list="student-list"
+                  placeholder="None"
+                />
+                </div>
+                <datalist id="student-list">
+                    {studentData.map(e =>
+                      <option>{e.rollNumber}</option>
+                    )}
+                  </datalist>
+                
+                <div>
+                  <button type='submit'>Edit Project</button>
                 </div>
               </form>
               <span>
                 <AiFillCloseCircle
                   size='1.7rem'
-                  onClick={() => toggleModel("update")}
+                  onClick={() => toggleModel(null)}
                 />
               </span>
               <button
                 className='close-data'
-                onClick={() => toggleModel("update")}
+                onClick={() => toggleModel(null)}
               >
                 Close
               </button>
