d63005ffd9cb9c9879e56346f467083d89e14e78
AliAkbar001 <786aliakbar.aa@gmail.com>
2022-01-22 00:50:29 +0500

    event-functionality-complete

--- custom-frontend/src/dashboard_pmo/events.js
+++ custom-frontend/src/dashboard_pmo/events.js
@@ -9,7 +9,11 @@ import { eventUrl } from "../apis";
 export default function Events() {
   const [getData, setGetData] = useState(false);
   const [editEvent, setEditEvent] = useState(false);
+  const [addEvent, setAddEvent] = useState(false);
   const [displayData, setDisplayData] = useState(false);
+  const [searchBy, setSearchBy] = useState("Name");
+  const [searchValue, setSearchValue] = useState("name");
+  const [searchData, setSearchData] = useState("");
   const [refresh, setRefresh] = useState(false);
 
   const api = axios.create({
@@ -27,6 +31,23 @@ export default function Events() {
    });
 },[refresh]);
 
+function handleSearch(e){
+  var getValue = e.target.value;
+  if(getValue === "Name"){
+    setSearchValue("name");
+  }else if(getValue === "Date"){
+    setSearchValue("date");
+  }else if(getValue === "Venue"){
+    setSearchValue("venue");
+  }else if(getValue === "Year"){
+    setSearchValue("year");
+  }else if(getValue === "Semester"){
+    setSearchValue("semester");
+  }else if(getValue === "Details"){
+    setSearchValue("details");
+  }
+  setSearchBy(getValue);
+}
 function deleteEvent(id){
   var check = window.confirm("Are sure you want to delete the event");
   if (check) {
@@ -46,9 +67,13 @@ function deleteEvent(id){
 }
 
 function toggleModel(action,event){
-  setDisplayData(event);
+  let date = new Date(event.date);
+  setDisplayData({...event, convertDate:date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate()});
   if(action === "update"){
     editEvent ? setEditEvent(false) : setEditEvent(true);
+  }
+  if(action === "add"){
+    addEvent ? setAddEvent(false) : setAddEvent(true);
   }
 }
 
@@ -57,8 +82,35 @@ function handleChange(e){
   var value = e.target.value;
   setDisplayData({ ...displayData, [name]: value });
 }
-
-function handleSubmit(e){
+function handleAddSubmit(e){
+  e.preventDefault();
+  const options = {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(displayData),
+  };
+  fetch(`${eventUrl}`, options)
+    .then((res) => res.json())
+    .then(
+      (result) => {
+        if (result.err.code === 0) {
+          setRefresh(!refresh);
+          toggleModel("add",false);
+          alert("Event Create Successfully");
+        } else if (result.err.code === 11000) {
+          alert(
+            `This ${JSON.stringify(result.err.keyValue)} is already in use`
+          );
+        } else if (result.err.message) {
+          alert(result.err.message);
+        }
+      },
+      (error) => {
+        alert(error);
+      }
+    );
+}
+function handleEditSubmit(e){
   e.preventDefault();
   const options = {
     method: "put",
@@ -69,11 +121,10 @@ function handleSubmit(e){
     .then((res) => res.json())
     .then(
       (result) => {
-        console.log(result)
         if (result.err.code === 0) {
           setRefresh(!refresh);
           toggleModel("update",false);
-          alert("Project Update Successfully");
+          alert("Event Update Successfully");
         } else if (result.err.code === 11000) {
           alert(
             `This ${JSON.stringify(result.err.keyValue)} is already in use`
@@ -92,20 +143,22 @@ function handleSubmit(e){
     <>
       <div className='data-container'>
         <div className='data-container-top'>
-          <input type='search' placeholder={"Search Student By "} />
+          <input type='search' 
+          value={searchData}
+          onChange={(e) => setSearchData(e.target.value)}
+          placeholder={"Search Student By " + searchBy} />
 
-          <select>
-            <option value='Roll Number'>Roll Number</option>
+          <select onChange={handleSearch}>
             <option value='Name'>Name</option>
-            <option value='Batch'>Batch</option>
-            <option value='Section'>Section</option>
-            <option value='Department'>Department</option>
-            <option value='Email'>Email</option>
-            <option value='Phone'>Phone</option>
+            <option value='Date'>Date</option>
+            <option value='Venue'>Venue</option>
+            <option value='Year'>Year</option>
+            <option value='Semester'>Semester</option>
+            <option value='Details'>Details</option>
           </select>
-          {/* <button className='add-data-btn' onClick={() => setShowModal(true)}>
-            Add Student
-          </button> */}
+          <button className='add-data-btn' onClick={() => toggleModel("add",false)}>
+            Add New Event
+          </button>
         </div>
         {!getData ? (
           <div>
@@ -128,9 +181,7 @@ function handleSubmit(e){
             </thead>
             <tbody>
               {/* Call event data here */}
-              {getData.map((event) => {
-                const eventDate = new Date();
-                const myYear = eventDate.getFullYear();
+              {getData.filter((event) =>(event[searchValue].toString().indexOf(searchData) > -1)).slice(0).reverse().map((event) => {
                 let date = new Date(event.date);
                 return (
                   <tr key={event._id}>
@@ -143,7 +194,7 @@ function handleSubmit(e){
                         date.getDate()}
                     </td>
                     <td data-label="Venue">{event.venue}</td>
-                    <td data-label="Year">{myYear}</td>
+                    <td data-label="Year">{event.year}</td>
                     <td data-label="Semester">{event.semester}</td>
                     <td data-label="Details">{event.details}</td>
                     <td data-label="Options">
@@ -166,14 +217,14 @@ function handleSubmit(e){
           </table>
         )}
       </div>
-      {editEvent && displayData && (
+      {addEvent && (
          <div className='popup-container'>
          <div className='popup'>
-           <h2>Edit Event</h2>
+           <h2>Create a New Event</h2>
            <div className='form-modal'>
              <form
                className='data-form'
-               onSubmit={handleSubmit}
+               onSubmit={handleAddSubmit}
                autoComplete='off'
                id='student-form'
              >
@@ -238,18 +289,113 @@ function handleSubmit(e){
                  />
                </div>
                <div>
+                 <button type='submit'>Save Event</button>
+               </div>
+             </form>
+             <span>
+               <AiFillCloseCircle
+                 size='1.7rem'
+                 onClick={() => toggleModel("add",false)}
+               />
+             </span>
+             <button
+               className='close-data'
+               onClick={() => toggleModel("add",false)}
+             >
+               Close
+             </button>
+             <form />
+           </div>
+         </div>
+       </div>
+      )}
+      {editEvent && displayData && (
+         <div className='popup-container'>
+         <div className='popup'>
+           <h2>Edit Event</h2>
+           <div className='form-modal'>
+             <form
+               className='data-form'
+               onSubmit={handleEditSubmit}
+               autoComplete='off'
+               id='student-form'
+             >
+               <input type='text' name='_id' value={displayData._id} hidden />
+               <div>
+                 <label>Event Name</label>
+                 <select 
+                   name='name'
+                   value={displayData.name}
+                   onChange={handleChange}
+                  >
+                  <option value="Defense">Defense</option>
+                  <option value="Mid Evaluation">Mid Evaluation</option>
+                  <option value="Final Evaluation">Final Evaluation</option>
+                 </select>
+               </div>
+               <div>
+                 <label>Current Date: { displayData.convertDate }</label>
+               </div>
+               <div>
+                 <label>Event Date</label>
+                 <input 
+                   type="date"
+                   name="date" 
+                   value={displayData.date} 
+                   onChange={handleChange}
+                 />
+               </div>
+                <div>
+                 <label>Venue</label>
+                 <input
+                   type='text'
+                   name='venue'
+                   value={displayData.venue}
+                   onChange={handleChange}
+                 />
+               </div>
+               <div>
+                 <label>Year</label>
+                 <input
+                   type='number'
+                   name='year'
+                   value={displayData.year}
+                   onChange={handleChange}
+                 />
+               </div>
+               <div>
+                 <label>Semester</label>
+                 <select 
+                   name='semester'
+                   value={displayData.semester}
+                   onChange={handleChange}
+                  >
+                  <option value="Fall">Fall</option>
+                  <option value="Spring">Spring</option>
+                 </select>
+               </div>
+               <div>
+                 <label className="event-details">Details</label>
+                 <textarea
+                   type='text'
+                   name='details'
+                   value={displayData.details}
+                   onChange={handleChange}
+                 />
+               </div>
+               <div>
                  <button type='submit'>Edit Event</button>
                </div>
              </form>
              <span>
                <AiFillCloseCircle
                  size='1.7rem'
-                 onClick={() => toggleModel("update",null)}
+                 onClick={() => toggleModel("update",false)}
                />
              </span>
              <button
                className='close-data'
-               onClick={() => toggleModel("update",null)}
+               onClick={() => toggleModel("update",false)}
              >
                Close
              </button>
