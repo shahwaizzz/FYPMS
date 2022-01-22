ff211e9119b3eb3eb45c162ce86f2c5b89aedc0d
Shahwaiz Naveed <shahwaiznaveed46@gmail.com>
2022-01-21 21:29:51 -0800

    App js refactor

--- custom-frontend/src/App.js
+++ custom-frontend/src/App.js
@@ -1,170 +1,38 @@
-// import React, { useState } from "react";
-// import "./login/index.css";
-// // import { useSelector } from "react-redux";
-// // import Dashoard_Pmo from "./dashboard_pmo/Dashboard_Pmo";
-// // import Dashoard_std from "./dashboard_student/Dashboard_std";
-// // import Dashoard_supervisor from "./dashboard_supervisor/Dashboard_supervisor";
-// import Adm_login from "./login/Adm_login";
-// // <<<<<<< HEAD
-// import Thr_login from "./login/Thr_login";
-// import Std_login from "./login/Std_login";
-// import Modal from "./components/modal";
-// // import Document from "./components/main-components/Documents";
-// import SSidebar from "./supervisor/sidebar";
-// // import { MdGroup } from "react-icons/md";
-// import Groups from './supervisor/group';
-// import ManageProjects from './supervisor/ManageProjects';
-// import ProjectProgress from "./supervisor/ProjectProgress";
-// import {Routes, Route} from 'react-router-dom'
-// // function handleValidation(params) {}
-// export default function App() {
-//   // Commit Test
-//   const [validation, setValidation] = useState(true);
-//   const [showModal, setShowModal] = useState(false);
-//   if (validation) {
-//     return (
-//       <div className='App'>
-//         <Navbar />
-//         {/* <button onClick={setShowModal}>Click Me</button>
-//         {showModal && <Modal />}
-//         <Routes>
-         
-//           <Route path="/supervisor" element={<SSidebar name="Home" abc={<Home />} />} />
-//           <Route path="/supervisor/updatepassword" element={<SSidebar name="Update Password" abc={<UserProfile />} />} />
-//           <Route path="/supervisor/createproject" element={<SSidebar name="Crreate Project" abc={<Groups />} />} />
-//           <Route path="/supervisor/manageprojects" element={<SSidebar name="Manage Projects" abc={<ManageProjects />} />} />
-//           <Route path="/supervisor/progress/:id" element={<SSidebar name="Progress" abc={<ProjectProgress />} />} />
-//         </Routes> */}
-
-//       </div>
-//     );
-//   } else {
-//     return (
-//       <div className='App'>
-//         {/* <Login /> */}
-//         <Routes>
-//           <Route path='/auth/pmo' element={<Adm_login />} />
-//           <Route path='/auth/supervisor' element={<Thr_login />} />
-//           <Route path='/auth/student' element={<Std_login />} />
-//           <Route
-//             path='/admin'
-//             element={
-//               <>
-//                 <Navbar />
-//                 <Sidebar name='Home' abc={<Home />} />
-//               </>
-//             }
-//           />
-//         </Routes>
-//       </div>
-//     );
-//   }
-// }
+import React, { useState } from "react";
+import "./login/index.css";
+import { useSelector } from "react-redux";
+import Dashoard_Pmo from "./dashboard_pmo/Dashboard_Pmo";
+import Dashoard_std from "./dashboard_student/Dashboard_std";
+import Dashoard_supervisor from "./dashboard_supervisor/Dashboard_supervisor";
+import Adm_login from "./login/Adm_login";
 // function handleValidation(params) {}
-// function App() {
-//   // Commit Test
-//   // const { pmo, student, supervisor } = useSelector((state) => state.AuthReducer)
-//   // if (student) {
-//   //   <div className="App">
-//   //       <Dashoard_supervisor />
-//   //     </div>
-//   // }
-//   // if (pmo) {
-//   return (
-//     <div className='App'>
-//       <Dashoard_Pmo />
-//     </div>
-//   );
-//   // } if (supervisor) {
-//   //   return (
-//   //     <div className="App">
-//   //       <Dashoard_std />
-//   //     </div>
-//   //   )
-//   // } else {
-//   //   return (
-//   //     <div className="App">
-//   //       <Adm_login />
-//   //     </div>
-//   //   )
-//   // }
-// }
-
-// export default App
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function App() {
+  // Commit Test
+  // const { pmo, student, supervisor } = useSelector((state) => state.AuthReducer)
+  // if (student) {
+  //   <div className="App">
+  //       <Dashoard_supervisor />
+  //     </div>
+  // }
+  // if (pmo) {
+  return (
+    <div className='App'>
+      <Dashoard_Pmo />
+    </div>
+  );
+  // } if (supervisor) {
+  //   return (
+  //     <div className="App">
+  //       <Dashoard_std />
+  //     </div>
+  //   )
+  // } else {
+  //   return (
+  //     <div className="App">
+  //       <Adm_login />
+  //     </div>
+  //   )
+  // }
+}
+
+export default App;
